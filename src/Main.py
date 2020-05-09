97764cd7f3daa5025b273d1e3889e8bcbe25eb72
ValliereMagic <louisemelaney@outlook.com>
2020-05-09 12:30:49 -0400

    Add functionality of calculating a bedtime with the amount of time to sleep given, and the time needed to wake up; change style of error catching in calculate_time_from and calculate_time_until to minimize unnecessary indentation; added additional commentary.

--- src/Main.py
+++ src/Main.py
@@ -26,40 +26,79 @@ def main():
                                                                                "Using today at midnight. After 3, "
                                                                                "The calculation will be done using "
                                                                                "tomorrow.")
+    arg_parser.add_argument('-s', '--sleep-for', dest="user_sleep_length", help="How long you want to sleep in hours:minutes, "
+                                                                                "and when you need to wake up.")
+    arg_parser.add_argument('-w', '--wake-up', dest="user_wake_time",
+                            help="When you want to wake up, in military time")
     # retrieve user time string from argument parser
     parsed_args: Namespace = arg_parser.parse_args()
     # execute the option the user specified
     time_from: str = parsed_args.user_time_from
     time_until: str = parsed_args.user_time_until
+    time_to_sleep: str = parsed_args.user_sleep_length
+    time_wake_up: str = parsed_args.user_wake_time
+    # Calculating amount of time from now
     if time_from is not None:
-        # parse the user provided into a timedelta
-        parsed_user_time: timedelta or None = parse_input_time(time_from)
         # execute the action specified by the user
-        execution_success: bool = calculate_time_from(parsed_user_time)
+        execution_success: bool = calculate_time_from(
+            parse_input_time(time_from))
         # if the action fails, display the help message
         if not execution_success:
             arg_parser.print_help()
+    # Calculating when to go to bed
+    elif (time_to_sleep is not None) or (time_wake_up is not None):
+        # Make sure that both arguments have been considered.
+        if (time_to_sleep is not None) and (time_wake_up is not None):
+            execution_success: bool = calculate_bedtime(parse_input_time(time_to_sleep),
+                                                        parse_input_time(time_wake_up))
+            if not execution_success:
+                arg_parser.print_help()
+        # User did not supply the required arguments
+        else:
+            print("Error. Both --sleep-for and --wake-up need to be defined "
+                  "to calculate when to wake up.")
+            arg_parser.print_help()
+
+    # Calculating between then and now
     elif time_until is not None:
-        parsed_user_time: timedelta or None = parse_input_time(time_until)
-        execution_success: bool = calculate_time_until(parsed_user_time)
+        # execute the action specified by the user
+        execution_success: bool = calculate_time_until(
+            parse_input_time(time_until))
+        # if the action fails, display the help message
         if not execution_success:
             arg_parser.print_help()
     else:
         arg_parser.print_help()
 
 
+def calculate_bedtime(sleep_length: timedelta, wake_up_time: timedelta) -> bool:
+    """
+    Calculate when you need to go to bed
+    """
+    if (sleep_length is None) or (wake_up_time is None):
+        return False
+    # The time we want to wake up, less the length of time to sleep
+    bedtime: timedelta = wake_up_time - sleep_length
+    # We don't care about crossing days, if we have reset the days to 0
+    if bedtime.days < 0:
+        bedtime += timedelta(days=1)
+    # Show the user when to go to bed
+    print("Bed Time: " + str(bedtime))
+    return True
+
+
 def calculate_time_from(user_time_arg: timedelta) -> bool:
     """
     Calculate the amount of time that is timedelta from now.
     """
-    if user_time_arg is not None:
-        # get the current time
-        current_time: datetime = datetime.now()
-        # calculate requested time
-        print("Time " + str(user_time_arg) + " from now: " + str((current_time + user_time_arg).time()
-                                                                 .strftime("%I:%M:%S %p")))
-        return True
-    return False
+    if user_time_arg is None:
+        return False
+    # get the current time
+    current_time: datetime = datetime.now()
+    # calculate requested time
+    print("Time " + str(user_time_arg) + " from now: " + str((current_time + user_time_arg).time()
+                                                             .strftime("%I:%M:%S %p")))
+    return True
 
 
 def calculate_time_until(user_time_arg: timedelta) -> bool:
@@ -68,24 +107,24 @@ def calculate_time_until(user_time_arg: timedelta) -> bool:
     If the function is executed before 3AM it will use today to calculate.
     If after 3AM it will use tomorrow.
     """
-    if user_time_arg is not None:
-        # using current time for starting point.
-        right_now = datetime.now()
-        # If before 3AM on current day, calculate using today at midnight instead.
-        # Otherwise user tomorrow at midnight.
-        if right_now.hour <= 3:
-            # today at midnight
-            until_time: datetime = datetime.combine(
-                datetime.today().date(), time(0, 0))
-        else:
-            # tomorrow at midnight
-            until_time: datetime = datetime.combine(
-                datetime.today().date(), time(0, 0)) + timedelta(days=1)
-        amount_of_time: timedelta = (until_time + user_time_arg) - right_now
-        print("Amount of time until " + str(user_time_arg) + " " + str(amount_of_time.seconds // 3600) +
-              "H " + str((amount_of_time.seconds // 60) % 60) + "M")
-        return True
-    return False
+    if user_time_arg is None:
+        return False
+    # using current time for starting point.
+    right_now = datetime.now()
+    # If before 3AM on current day, calculate using today at midnight instead.
+    # Otherwise user tomorrow at midnight.
+    if right_now.hour <= 3:
+        # today at midnight
+        until_time: datetime = datetime.combine(
+            datetime.today().date(), time(0, 0))
+    else:
+        # tomorrow at midnight
+        until_time: datetime = datetime.combine(
+            datetime.today().date(), time(0, 0)) + timedelta(days=1)
+    amount_of_time: timedelta = (until_time + user_time_arg) - right_now
+    print("Amount of time until " + str(user_time_arg) + " " + str(amount_of_time.seconds // 3600) +
+          "H " + str((amount_of_time.seconds // 60) % 60) + "M")
+    return True
 
 
 def parse_input_time(arg: str) -> timedelta or None:
